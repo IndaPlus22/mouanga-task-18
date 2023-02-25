e67328b50c66cbe914583ea5b98a21169afeca6b
salticecream <shortgolind@gmail.com>
2023-02-25 12:01:08 +0100

    add power string

--- power strings/src/main.rs
+++ power strings/src/main.rs
@@ -4,44 +4,48 @@ const ZERO: i32 = 0;
 fn main(){
     loop {
         let input = get_input();
-        if input.eq(&vec![46]) {
+        if input == String::from(".") {
             return;
         } else {
             println!("{}", cps(&input));
         }
     }
+    
 }
 
-fn get_input() -> Vec<u8> {
+fn get_input() -> String {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("lmao");
-    return input.trim().as_bytes().to_vec();
+    return String::from(input.trim());
 }
 
-// calculate power string
-fn cps(input: &[u8]) -> usize {
-
-    let len = input.len();
-    let mut letters_used: [bool; 26] = [false; 26];
-    let mut current_word: Vec<u8> = vec![];
-
-    for letter in input {
-        // this letter has already been used; string could be repeating from this point on
-        if letters_used[*letter as usize - 97] {
-            let mut equality = true;
-            println!("Testing!");
-            for _letter in &current_word {
-                if _letter != letter {
-                    equality = false;
-                    break;
-                }
-            if equality {
-                return len / current_word.len();
+fn cps(string: &String) -> usize {
+    let strlen = string.len();
+    // we only need to check substrings with a length dividing the length of the input string
+                            // add 1 here to avoid runtime error when strlen is 1 and iterator min > iterator max
+    for sublen in 1..(1 + strlen/2) {
+        if strlen % sublen != 0 {
+            continue;
+        } else {
+            let string_to_check = &string[..sublen];
+            if check_string(string, string_to_check) {
+                // strlen is equal to sublen * the exponent
+                return strlen/sublen;
             }
         }
-        }
-        // note: 97 is the ascii code point for 'a'
-        letters_used[*letter as usize - 97] = true;
     }
-    69000
+
+    return 1;
+}
+
+fn check_string(string: &str, substring: &str) -> bool {
+    let mut start: usize = 0;
+    let mut counter = 0;
+    while let Some(index) = string[start..].find(substring) {
+        start += index;
+        start += substring.len();
+        counter += 1;
+    }
+    // no need to check for fractions since we already promised that string % substring == 0
+    counter == string.len() / substring.len()
 }
